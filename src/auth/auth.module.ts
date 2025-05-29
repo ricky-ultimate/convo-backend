5e689ffa1751b56bbcb23380682c1d91fa9c02bf
Ricky Ultimate <rickyogigi@gmail.com>
2025-05-29 15:14:54 +0100

    chore: import redis

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { PrismaModule } from '../prisma/prisma.module'; // For Prisma access
+import { PrismaModule } from '../prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
   imports: [
-    PrismaModule, // Prisma service is available here
+    PrismaModule,
     PassportModule,
+    RedisModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET, // Use env secret
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1h' },
     }),
   ],
