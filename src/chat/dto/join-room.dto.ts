746d15ceec8840ddcb50829e6a25fb69a42b832b
Ricky Ultimate <rickyogigi@gmail.com>
2025-05-29 18:31:25 +0100

    feat: change room name to id

--- src/chat/dto/join-room.dto.ts
+++ src/chat/dto/join-room.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class JoinRoomDto {
   @IsString()
-  @IsNotEmpty({ message: 'Room name is required' })
-  roomName: string;
+  @IsNotEmpty({ message: 'Room ID is required' })
+  @IsUUID('4', { message: 'Room ID must be a valid UUID' })
+  roomId: string;
 }
